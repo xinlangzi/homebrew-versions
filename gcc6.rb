16a316f8665e95324f3322b9d36b8e7ea8ea13a6
Andrew Lee <leeas@stolaf.edu>
2016-09-17 14:27:28 -0500

    gcc6 6.2.0
    Closes #1386.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- gcc6.rb
+++ gcc6.rb
@@ -21,9 +21,9 @@ class Gcc6 < Formula
 
   desc "The GNU Compiler Collection"
   homepage "https://gcc.gnu.org"
-  url "https://ftpmirror.gnu.org/gcc/gcc-6.1.0/gcc-6.1.0.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/gcc/gcc-6.1.0/gcc-6.1.0.tar.bz2"
-  sha256 "09c4c85cabebb971b1de732a0219609f93fc0af5f86f6e437fd8d7f832f1a351"
+  url "https://ftpmirror.gnu.org/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2"
+  sha256 "9944589fc722d3e66308c0ce5257788ebd7872982a718aa2516123940671b7c5"
 
   bottle do
     sha256 "96278ee4d2affeb20cf2a26b84ef456694e0ced88551c15a9d82c721fdf63e0d" => :el_capitan
