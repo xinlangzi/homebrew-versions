ebefcf76ddd588aee9076f794ba2989f2c01fdec
Mike McQuaid <mike@mikemcquaid.com>
2016-10-01 17:38:05 +0100

    ruby192: revision bump for readline upgrade.

--- ruby192.rb
+++ ruby192.rb
@@ -3,6 +3,7 @@ class Ruby192 < Formula
   homepage "http://www.ruby-lang.org/en/"
   url "http://cache.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p330.tar.bz2"
   sha256 "6d3487ea8a86ad0fa78a8535078ff3c7a91ca9f99eff0a6a08e66c6e6bf2040f"
+  revision 1
 
   depends_on "pkg-config" => :build
   depends_on "readline"
