dfa7db9e5578751b8b87cd0d2d892239e02ff12b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 20:41:53 +0100

    guile18: add 1.8.8_1 bottle.

--- guile18.rb
+++ guile18.rb
@@ -4,6 +4,12 @@ class Guile18 < Formula
   mirror "https://ftp.gnu.org/gnu/guile/guile-1.8.8.tar.gz"
   sha256 "c3471fed2e72e5b04ad133bbaaf16369e8360283679bcf19800bc1b381024050"
 
+  bottle do
+    sha256 "f6cc7fc10e96c8a0f3f9995d553928e59c4cbb009ddc474cd9e4f46987b4a309" => :sierra
+    sha256 "870763907c7b555f280cd68e816789490105318d5a5ff40f6ad4f463ce87cdb0" => :el_capitan
+    sha256 "82b956361a7353c23e1b7109f7cf1b5f4bf3c723e47ffca9283688d67d339508" => :yosemite
+  end
+
   revision 1
 
   depends_on "pkg-config" => :build
