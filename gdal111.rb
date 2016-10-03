53329833062d3fb9eb4ea82f5b292b097d72b81a
Andrew Janke <andrew@apjanke.net>
2016-10-03 12:45:20 -0400

    gdal111: revision bump for proj

--- gdal111.rb
+++ gdal111.rb
@@ -1,6 +1,7 @@
 class Gdal111 < Formula
   desc "GDAL: Geospatial Data Abstraction Library"
   homepage "http://www.gdal.org/"
+  revision 1
 
   stable do
     url "http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz"
